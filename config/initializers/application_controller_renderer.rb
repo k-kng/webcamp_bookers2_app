d4ab25adabd77b773926c4e023443f18cf092b55
k-kng <ec2-user@ip-172-31-34-40.ec2.internal>
2021-08-25 11:34:34 +0000

    [Finish]5-10

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '258ad5eaee6978723c354ff053b707d3de4fe260268b4b41e8c2780bdca2dc0f19398da764333ef09aa15b32ce6e353e860031950844663c9e9f233a2e3fc48b'
